d760b878d26607d50407cd5427f4919b20a40f80
lindapei2016 <lindapei2016@gmail.com>
2024-06-26 18:21:57 -0700

    Updated readme and created Jupyter notebook for lab.

--- Examples_QuickStartSimulation_LPtest.py
+++ Examples_QuickStartSimulation_LPtest.py
@@ -3,36 +3,7 @@
 # Examples_QuickStartSimulation.py
 # This document contains examples of how to use the simulation code.
 
-# To launch the examples, either
-# (1) Run the following command in the OS command line or Terminal:
-#   python3 Examples_QuickStartSimulation.py
-# (2) Copy and paste the code of this document into an interactive
-#   Python console.
 
-# Note that if modules cannot be found, this is a path problem.
-# In both fixes below, replace <NAME_OF_YOUR_DIRECTORY> with a string
-#   containing the directory in which the modules reside.
-# (1) The following path can be updated via the OS command line or
-#   Terminal (e.g. on a cluster):
-#   export PYTHONPATH=<NAME_OF_YOUR_DIRECTORY>:$PYTHONPATH
-# (2) The following code can be added to the main .py script file
-#   (e.g. can be added to the top of this document):
-#   import sys
-#   sys.path.append(<NAME_OF_YOUR_DIRECTORY>)
-
-###############################################################################
-
-# Import other code modules
-# SimObjects contains classes of objects that change within a simulation
-#   replication.
-# DataObjects (generally) contains classes of objects that do not change
-#   within simulation replications and also do not change across
-#   simulation replications -- they contain data that are "fixed"
-#   for an overall problem. (Note: EpiParams is an exception -- it is
-#   kind of a hybrid.)
-# SimModel contains the class SimModel, which runs
-#   a simulation replication and stores the simulation data
-#   in its object attributes..
 
 from Engine_SimObjects import MultiTierPolicy
 from Engine_DataObjects import DataPrepConfig, TierInfo
@@ -59,15 +30,26 @@ austin_model = sim_model_constructor.create_sim_model()
 
 tiers = TierInfo(austin_dict_filenames["tier_info_json"])
 
-thresholds = (-1, 100, 200, 500, 1000)
+thresholds = (-1, 100, 200, 500)
 mtp = MultiTierPolicy(tiers, thresholds)
 
 austin_model.policy = mtp
 
+austin_model.fixed_kappa_end_date = 0
 austin_model.simulate_time_period(945)
 
 print(austin_model.compute_rsq())
 print(np.sum(austin_model.ICU_history))
+
+austin_model.reset()
+austin_model.policy.reset()
+austin_model.fixed_kappa_end_date = 0
+austin_model.simulate_time_period(945)
+
+austin_model.reset()
+austin_model.policy.reset()
+austin_model.fixed_kappa_end_date = 0
+austin_model.simulate_time_period(945)
 
 print(time.time() - start_time)
 
--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # SEIRModelComponents
+
+An example Jupyter Notebook, demo_for_discussion.ipynb, has been made available as a code tutorial.
+
+## Requirements
+- Python 3 (this code was built using Python 3.4)
+- numpy, pandas
+- pathlib (comes automatically with Python >= 3.4)
+- json (automatically included in Python)
+
+## Overview of modules:
+- SimObjects contains classes of objects that change within a simulation replication.
+- DataObjects (generally) contains classes of objects that do not change
+  within simulation replications and also do not change across
+  simulation replications -- they contain data that are "fixed"
+  for an overall problem. (Note: EpiParams is an exception -- it is
+  kind of a hybrid.)
+- SimModel contains the class SimModel, which runs
+  a simulation replication and stores the simulation data
+  in its object attributes..
+
+## Known bugs:
+- The stepsize is accidentally 9, not 10 (so that we are splitting a day into 9 steps for discretization)
+- The update for S seems suspicious (potentially a typo)
+- Booster doses take effect immediately, whereas the first and second vaccine dose take time to become effective
+- Fixed_kappa_end_date seems to be buggy
+
+## Things we need to do:
+- Unit tests and integration tests
+- Need function signatures and parameter definitions for all classes and methods
+- Write least-squares (calibration), optimization, and input/output (importing/exporting intermediate simulation states) 
+to be compatible with this code version
+- Incorporate new automatic calibration ideas/code (from Guyi)
+- Incorporate wastewater code (wastewater code updates have diverged significantly)
+- Fix messy data inputs -- there are too many .csv and .json files 
+and the dictionary formats are not consistent -- this leads to unreadable code in parsing/loading
+the data
+
+Note that if modules cannot be found, this is a path problem.
+In both fixes below, replace <NAME_OF_YOUR_DIRECTORY> with a string
+  containing the directory in which the modules reside.
+
+(1) The following path can be updated via the OS command line or
+  Terminal (e.g. on a cluster):
+  export PYTHONPATH=<NAME_OF_YOUR_DIRECTORY>:$PYTHONPATH
+
+(2) The following code can be added to the main .py script file
+  (e.g. can be added to the top of this document):
+  import sys
+  sys.path.append(<NAME_OF_YOUR_DIRECTORY>)
+
+
